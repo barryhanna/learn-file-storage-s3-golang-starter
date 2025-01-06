a9ef8a74ec2f0d48f99f635fa40b2f9405d1556e
Barry Hanna <barryahanna@gmail.com>
2025-01-06 08:40:36 +0000

    change thumbnail back to an assets URL

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
@@ -44,11 +46,11 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	}
 	defer file.Close()
 	mediaType := header.Header.Get("Content-Type")
-	imageData, err := io.ReadAll(file)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Unable open image file", err)
-		return
-	}
+	// imageData, err := io.ReadAll(file)
+	// if err != nil {
+	// 	respondWithError(w, http.StatusBadRequest, "Unable open image file", err)
+	// 	return
+	// }
 	video, err := cfg.db.GetVideo(videoID)
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Unable get image meta data", err)
@@ -61,8 +63,21 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	// videoThumbnail := thumbnail{data: imageData, mediaType: mediaType}
 	// videoThumbnails[videoID] = videoThumbnail
 
-	// formattedThumbnailUrl := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
-	formattedThumbnailUrl := fmt.Sprintf("data:%s;base64,%s", mediaType, base64.StdEncoding.EncodeToString(imageData))
+	fileExtension := strings.Split(mediaType, "/")[1]
+	formattedThumbnailUrl := fmt.Sprintf("http://localhost:%s/assets/%s.%s", cfg.port, videoID, fileExtension)
+	// formattedThumbnailUrl := fmt.Sprintf("data:%s;base64,%s", mediaType, base64.StdEncoding.EncodeToString(imageData))
+	path := filepath.Join(cfg.assetsRoot, videoID.String()+"."+fileExtension)
+	outFile, err := os.Create(path)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Problem saving thumnail file to filesystem", err)
+		return
+	}
+	defer file.Close()
+
+	if _, err := io.Copy(outFile, file); err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Problem saving thumnail file to filesystem", err)
+		return
+	}
 	video.ThumbnailURL = &formattedThumbnailUrl
 	cfg.db.UpdateVideo(video)
 	respondWithJSON(w, http.StatusOK, database.Video(video))
